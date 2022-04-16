c253055cd00d9f2710575ab47df30be302b5d297
srv176 <36818756+srv883@users.noreply.github.com>
2022-04-16 11:59:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+input("please hit enter to toss the coin")
+Coin_toss = random.randint(0,1)
+if Coin_toss == 0:
+ print("its a head")
+else:
+  print("its a tails")
+
+
+
+
+
+
+
+
+
+
+
